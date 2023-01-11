1973622ee86966d4e1cdf972b0e1584030fa6ae5
Akshay <AkshayAwate>
2023-01-11 10:32:28 +0530

    update: added values.yaml

---
+++ security-groups.tf
@@ -0,0 +1,23 @@
+module "vote_service_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "memorydb-sg"
+  description = "Security group for memorydb"
+  vpc_id      = module.vpc.default_vpc_id
+
+  ingress_cidr_blocks      = ["10.10.0.0/16"]
+  ingress_rules            = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8090
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "10.10.0.0/16"
+    },
+    {
+      rule        = "postgresql-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+}
